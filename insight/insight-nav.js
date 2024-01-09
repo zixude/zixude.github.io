56beac4fa64d6b530665d5c4a6581d6379296db2
kindyou <kxd@live.com>
2024-01-09 11:08:30 +0800

    new

--- insight/insight-nav.js
+++ insight/insight-nav.js
@@ -6,10 +6,14 @@ container.id = "insight-catalogs";
 
 // 创建链接数组
 var links = [
-  { text: "#记忆碎片", url: "../insight/记忆碎片.html" },
-  { text: "#乐之本事", url: "../insight/乐之本事.html" },
-  { text: "#梦之演绎", url: "../insight/梦之演绎.html" },
-  { text: "#时间之伤", url: "../insight/时间之伤.html" },
+  { text: "一辑", url: "../insight/一辑.html" },
+  { text: "二辑", url: "../insight/二辑.html" },
+  { text: "三辑", url: "../insight/三辑.html" },
+  { text: "四辑", url: "../insight/四辑.html" },
+  { text: "五辑", url: "../insight/五辑.html" },
+  { text: "六辑", url: "../insight/六辑.html" },
+  { text: "七辑", url: "../insight/七辑.html" },
+  { text: "八辑", url: "../insight/八辑.html" },
 ];
 
 var linksHTML = ""; // 存储链接元素的 HTML 字符串
--- think/think-nav.js
+++ think/think-nav.js
@@ -18,30 +18,23 @@ document.body.appendChild(container);
 
 // 创建链接数组
 var links = [
-  { text: "纳瓦尔思想", url: "纳瓦尔思想.html" },
-  { text: "认知构建", url: "认知构建.html" },
-  { text: "信息管理", url: "信息管理.html" },
-  { text: "文学写作要则", url: "文学写作要则.html" },
-  { text: "英语的思维", url: "乞讨.html" },
-  { text: "互动叙事研究", url: "乞讨.html" },
-  { text: "写作技巧", url: "乞讨.html" },
-  { text: "电影表达方式研究", url: "乞讨.html" },
-  { text: "信息管理", url: "乞讨.html" },
-  { text: "自动化系统", url: "乞讨.html" },
-  { text: "自我", url: "乞讨.html" },
-  { text: "科技对现代政治的围猎", url: "乞讨.html" },
-  { text: "保卫人性", url: "乞讨.html" },
-  { text: "雪中故事", url: "乞讨.html" },
-  { text: "生活的乞讨", url: "乞讨.html" },
-  { text: "论光速不变性", url: "乞讨.html" },
-  { text: "AI使幻想暗淡无光", url: "乞讨.html" },
+  { text: "文学写作要则" },
+  { text: "博客哲学" },
+  { text: "极简主义" },
+  { text: "认知构建" },
+  { text: "信息的启发与关联" },
+  { text: "信息管理" },
+  { text: "Navalism" },
+  { text: "减肥要点" },
+  // 其他链接...
 ];
 
 var linksHTML = ""; // 存储链接元素的 HTML 字符串
 
 // 生成链接元素的 HTML 字符串
 links.forEach(function(link) {
-  linksHTML += `<a href="${link.url}">${link.text}</a><br>`;
+  var url = link.text.replace(/ /g, "-").toLowerCase() + ".html";
+  linksHTML += `<a href="${url}">${link.text}</a><br>`;
 });
 
 // 将链接元素的 HTML 字符串添加到容器元素中
