4ace0acdc292539d5be1198efab15f62fd9f008c
kindyou <kxd@live.com>
2024-03-08 18:41:55 +0800

    /

--- app/state.js
+++ app/state.js
@@ -1,22 +1,40 @@
 
 
-/////////////////////////////////////////////////////////////////////////////////
+// 片段一：给页面添加mata UTF-8的声明
+
+// 创建 meta 元素
+var metaTag = document.createElement('meta');
+
+// 设置 meta 元素的属性
+metaTag.setAttribute('charset', 'UTF-8');
+
+// 获取 head 元素
+var headTag = document.getElementsByTagName('head')[0];
+
+// 将 meta 元素添加到 head 元素中
+headTag.appendChild(metaTag);
 
 
-    // 创建 meta 元素
-    var metaTag = document.createElement('meta');
-  
-    // 设置 meta 元素的属性
-    metaTag.setAttribute('charset', 'UTF-8');
-  
-    // 获取 head 元素
-    var headTag = document.getElementsByTagName('head')[0];
-  
-    // 将 meta 元素添加到 head 元素中
-    headTag.appendChild(metaTag);
 
 
-/////////////////////////////////////////////////////////////////////////////////
+// 片段二：给页面添加mata title的声明
+
+// 创建 title 元素
+var titleTag = document.createElement('title');
+
+// 设置 title 元素的文本内容
+titleTag.textContent = '自序｜璞成';
+
+// 获取 head 元素
+var headTag = document.getElementsByTagName('head')[0];
+
+// 将 title 元素添加到 head 元素中
+headTag.appendChild(titleTag);
+
+
+
+
+// 片段三：给页面添加支持按设备的宽度显示样式的代码
 
 // 创建 meta 元素
 var metaTag = document.createElement('meta');
@@ -32,18 +50,17 @@ var headTag = document.getElementsByTagName('head')[0];
 headTag.appendChild(metaTag);
 
 
-/////////////////////////////////////////////////////////////////////////////////
 
+
+// 片段四：给页面添加指向统一的css文件
 
 // 创建<link>标签并设置属性
 var link = document.createElement("link");
 link.rel = "stylesheet";
 link.href = "../app/styles.css";
 
-
 // 将创建的元素添加到<head>标签中
 var head = document.head || document.getElementsByTagName("head")[0];
 head.appendChild(link);
 head.appendChild(meta);
 
-/////////////////////////////////////////////////////////////////////////////////
